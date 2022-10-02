a5da2a17ac0c00f738b1c3ad2c3a779ec4227034
Diogo Rocha <diogo.rocha42@outlook.com>
2022-10-02 10:37:51 -0300

    Ajsutando card da imagem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import {React, Stack} from 'react';
+import { React, Stack } from 'react';
 import loadImage from './assets/dist/img/Reload-1s-200px.gif'
 
 import SelectItem from './components/SelectComponent';
 import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
 
 function App() {
-  
+
   function refreshPage() {
     window.location.reload(false);
   }
@@ -36,15 +36,15 @@ function App() {
         </div>
 
         <div className="navbar navbar-dark bg-dark shadow-sm">
-            <div className="container">
-              <a href="#" className="navbar-brand d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-                <strong>Free Images Generator</strong>
-              </a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </div>
+          <div className="container">
+            <a href="#" className="navbar-brand d-flex align-items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" /><circle cx="12" cy="13" r="4" /></svg>
+              <strong>Free Images Generator</strong>
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
         </div>
       </header>
 
@@ -55,10 +55,10 @@ function App() {
             <div className="col-lg-6 col-md-8 mx-auto">
               <h1 className="fw-light">Free random web image generator (in beta)</h1>
               <p className="lead text-muted">Grab images from the web in an easy and simple way, to assemble your presentations or to place on your website.</p>
-              
+
               <Button variant="primary" size='lg' onClick={refreshPage}>Refresh Images</Button>
 
-              <p style={{marginTop :15}}>Click the button above to reload the page and load more images!</p>
+              <p style={{ marginTop: 15 }}>Click the button above to reload the page and load more images!</p>
             </div>
           </div>
         </section>
@@ -66,22 +66,22 @@ function App() {
         <section className="text-center container">
           <div className="row py-lg-4">
             <div className="col-md-8 mx-auto">
-                <Button style={{marginLeft :10, marginTop :3}} variant="secondary">
-                  Animals 🐪
-                </Button>              
-                <Button style={{marginLeft :10, marginTop :3}} variant="secondary">
-                  Vehicles 🚗
-                </Button>              
-                <Button style={{marginLeft :10, marginTop :3}}  variant="secondary">
-                  Technology 💻
-                </Button>              
-                <Button style={{marginLeft :10, marginTop :3}} variant="secondary">
-                  Food 🌮
-                </Button>              
-                <Button style={{marginLeft :10, marginTop :3}} variant="secondary">
-                  Nature 🌳
-                </Button>
-              </div>
+              <Button style={{ marginLeft: 10, marginTop: 3 }} variant="secondary">
+                Animals 🐪
+              </Button>
+              <Button style={{ marginLeft: 10, marginTop: 3 }} variant="secondary">
+                Vehicles 🚗
+              </Button>
+              <Button style={{ marginLeft: 10, marginTop: 3 }} variant="secondary">
+                Technology 💻
+              </Button>
+              <Button style={{ marginLeft: 10, marginTop: 3 }} variant="secondary">
+                Food 🌮
+              </Button>
+              <Button style={{ marginLeft: 10, marginTop: 3 }} variant="secondary">
+                Nature 🌳
+              </Button>
+            </div>
           </div>
         </section>
 
@@ -102,68 +102,38 @@ function App() {
           <div className="container">
 
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-              <div className="col">
-                <div className="card shadow-sm">
-                  <div className="bd-example">  
-                    
-                    <div className="center">
-                      {/* <img id="img_home2" src="assets/dist/img/Reload-1s-200px.gif" className="card-img-top" alt="Loading..."> */}
-                      <img id="img_home2" src={loadImage} className="card-img-top" alt="Loading..."/>
-                    </div>
 
+              <div className="justify-content-between align-items-center">
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" id="img_home2" src={loadImage} />
+                  <div className="center">
+                    <Card.Body className="align-items-center" >
+                      <Button variant="outline-success">Go to Image</Button>
+                    </Card.Body>
                   </div>
-
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-center">
-                        <Button variant="outline-success">Go to Image</Button>
-                        {/* <button type="button" className="btn btn-outline-success" onClick="downloadImage2()">Go to Image</button> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                </Card>
               </div>
-              <div className="col">
-                <div className="card shadow-sm">
-                  <div className="bd-example">
 
-                    <div className="center">
-                      {/* <img id="img_home3" src="assets/dist/img/Reload-1s-200px.gif" className="card-img-top" alt="Loading..."> */}
-                      <img id="img_home3" src={loadImage} className="card-img-top" alt="Loading..."/>
-                    </div>
-                    
+              <div className="justify-content-between align-items-center">
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" id="img_home3" src={loadImage} />
+                  <div className="center">
+                    <Card.Body className="align-items-center" >
+                      <Button variant="outline-success">Go to Image</Button>
+                    </Card.Body>
                   </div>
-
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-center">
-                        <Button variant="outline-success">Go to Image</Button>
-                        {/* <button type="button" className="btn btn-outline-success" onClick="downloadImage3()">Go to Image</button> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                </Card>
               </div>
-              <div className="col">
-                <div className="card shadow-sm">
-                  <div className="bd-example">
-              
-                    <div className="center">
-                      {/* <img id="img_home1" src="assets/dist/img/Reload-1s-200px.gif" className="card-img-top" alt="Loading..."> */}
-                      <img id="img_home1" src={loadImage} className="card-img-top" alt="Loading..."/>
-                    </div>
 
+              <div className="justify-content-between align-items-center">
+                <Card style={{ width: '20rem' }}>
+                  <Card.Img variant="top" id="img_home1" src={loadImage} />
+                  <div className="center">
+                    <Card.Body className="align-items-center" >
+                      <Button variant="outline-success">Go to Image</Button>
+                    </Card.Body>
                   </div>
-
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-center">
-                        <Button variant="outline-success">Go to Image</Button>
-                        {/* <button type="button" className="btn btn-outline-success" onClick="downloadImage1()">Go to Image</button> */}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                </Card>
               </div>
 
             </div>
@@ -182,7 +152,7 @@ function App() {
       </footer>
 
     </div>
-    
+
   );
 }
 
